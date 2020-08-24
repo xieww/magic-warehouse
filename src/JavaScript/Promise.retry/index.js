48edf00843dd406cf2994ae0b765383159fd53b2
Chasen.xie <chasen.xie@trustasia.com>
2020-08-24 16:25:14 +0800

    feat(promise.retry): promise.retry

--- src/JavaScript/Promise.retry/index.js
+++ src/JavaScript/Promise.retry/index.js
@@ -1,23 +1,41 @@
-Promise.retry = function (callback, max) {
-  let counter = 0;
-  function execution() {
-    return new Promise((resolve, reject) => {
-      console.log(`正在执行第${counter}次重试`);
-      resolve(callback());
-    })
-      .then((res) => Promise.resolve(res))
-      .catch((err) => {
-        counter++;
-        if (counter >= max) {
-          console.log("抱歉，多次重试失败，程序退出执行！");
-          return Promise.reject(err);
-        } else {
-          return execution();
-        }
-      });
-  }
+//方法一
+// Promise.retry = function (callback, max) {
+//   let counter = 0;
+//   function execution() {
+//     return new Promise((resolve, reject) => {
+//       console.log(`正在执行第${counter}次重试`);
+//       resolve(callback());
+//     })
+//       .then((res) => Promise.resolve(res))
+//       .catch((err) => {
+//         counter++;
+//         if (counter >= max) {
+//           console.log("抱歉，多次重试失败，程序退出执行！");
+//           return Promise.reject(err);
+//         } else {
+//           return execution();
+//         }
+//       });
+//   }
 
-  return execution();
+//   return execution();
+// };
+
+// 方式二
+Promise.retry = function (fn, num) {
+  return new Promise(async (resolve, reject) => {
+    while (num > 0) {
+      num--;
+      try {
+        const response = await fn();
+        resolve(response);
+      } catch (error) {
+        if (!num) {
+          reject(error);
+        }
+      }
+    }
+  });
 };
 
 Promise.retry(() => {
