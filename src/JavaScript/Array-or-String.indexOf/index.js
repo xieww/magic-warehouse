b5b1a6d66c7e83d6cc0d7e7a9679c5bfc11b9ee5
Chasen.xie <chasen.xie@trustasia.com>
2020-08-26 09:46:46 +0800

    feat(JavaScript): 数组或字符串通用indexOf方法

---
+++ src/JavaScript/Array-or-String.indexOf/index.js
@@ -0,0 +1,45 @@
+function _indexOf(item, searchValue, start = 0) {
+  const DATA_TYPE = typeof item;
+  if (DATA_TYPE === "string") {
+    var regex = new RegExp(`${searchValue}`, "ig");
+    regex.lastIndex = start;
+    const result = regex.exec(item);
+    return result ? result.index : -1;
+  } else {
+    if (!searchValue) {
+      return -1;
+    }
+
+    for (; start < item.length; start++) {
+      if (item[start] === searchValue) {
+        return start;
+      }
+    }
+    return -1;
+  }
+}
+
+console.log("should be 2 ,result is ", _indexOf("sting in", "in"));
+console.log("should be -1 ,result is ", _indexOf("sting in", "in", 7));
+console.log("should be -1 ,result is ", _indexOf("sting in", "ings"));
+console.log("should be -1 ,result is ", _indexOf("sting in"));
+console.log(
+  "should be -1 ,result is ",
+  _indexOf(["hello", "world", "orange", "apple"])
+);
+console.log(
+  "should be 2 ,result is ",
+  _indexOf(["hello", "world", "orange", "apple"], "orange")
+);
+console.log(
+  "should be -1 ,result is ",
+  _indexOf(["hello", "world", "orange", "apple"], "orange", 3)
+);
+console.log(
+  "should be 4 ,result is ",
+  _indexOf(["hello", "world", "orange", "apple", "orange"], "orange", 3)
+);
+console.log(
+  "should be -1 ,result is ",
+  _indexOf(["hello", "world", "orange", "apple"], "oranges")
+);
