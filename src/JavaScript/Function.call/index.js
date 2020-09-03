8fedef284a592b920542aca870df6371fb71aca2
Chasen.xie <chasen.xie@trustasia.com>
2020-09-03 14:46:19 +0800

    feat(JavaScript): Function.call()实现

---
+++ src/JavaScript/Function.call/index.js
@@ -0,0 +1,41 @@
+/**
+ * 支持浏览器和node环境
+ * @param {*} context 
+ */
+Function.prototype._call = function (context) {
+  var context = context
+    ? context
+    : typeof window === "undefined"
+    ? global
+    : window;
+  context.fn = this;
+
+  var args = [];
+  for (var i = 1, len = arguments.length; i < len; i++) {
+    args.push("arguments[" + i + "]");
+  }
+
+  var result = eval("context.fn(" + args + ")");
+
+  delete context.fn;
+  return result;
+};
+
+// 测试
+var value = 2;
+var obj = {
+  value: 1,
+};
+
+function bar(name, age) {
+  console.log(this.value);
+  return {
+    value: this.value,
+    name: name,
+    age: age,
+  };
+}
+
+bar._call(null);
+
+console.log(bar._call(obj, "Jack Ma", 18));
