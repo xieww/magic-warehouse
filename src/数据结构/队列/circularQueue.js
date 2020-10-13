c13c5a8d8fe1d5f598b12b71c660646b99eb8e63
Chasen.xie <chasen.xie@trustasia.com>
2020-10-13 18:24:34 +0800

    feat(queue、linkList): 栈、队列、链表的模拟实现

--- src/数据结构/队列/circularQueue.js
+++ src/数据结构/队列/circularQueue.js
@@ -14,18 +14,34 @@ class CircularQueue {
     // 当前队列大小
     this.size = 0;
   }
-  enQueue(item) {
+
+  /**
+   * @description 将一个元素放入队列的尾部
+   * @author xieww
+   * @date 2020-10-13
+   * @param {*} item
+   * @memberof Queue
+   */
+  push(value) {
     // 判断队尾 + 1 是否为队头
     // 如果是就代表需要扩容数组
     // % this.queue.length 是为了防止数组越界
     if (this.first === (this.last + 1) % this.queue.length) {
       this.resize(this.getLength() * 2 + 1);
     }
-    this.queue[this.last] = item;
+    this.queue[this.last] = value;
     this.size++;
     this.last = (this.last + 1) % this.queue.length;
   }
-  deQueue() {
+
+  /**
+   * @description 从队列首部移除元素
+   * @author xieww
+   * @date 2020-10-13
+   * @returns
+   * @memberof Queue
+   */
+  pop() {
     if (this.isEmpty()) {
       throw Error("Queue is empty");
     }
@@ -41,18 +57,50 @@ class CircularQueue {
     }
     return r;
   }
-  getHeader() {
+
+  /**
+   * @description 返回队列首部的元素
+   * @author xieww
+   * @date 2020-10-13
+   * @returns
+   * @memberof Queue
+   */
+  peek() {
     if (this.isEmpty()) {
       throw Error("Queue is empty");
     }
     return this.queue[this.first];
   }
+
+  /**
+   * @description 获取队列长度
+   * @author xieww
+   * @date 2020-10-13
+   * @returns
+   * @memberof Queue
+   */
   getLength() {
     return this.queue.length - 1;
   }
+
+  /**
+   * @description 队列是否为空
+   * @author xieww
+   * @date 2020-10-13
+   * @returns {boolean}
+   * @memberof Queue
+   */
   isEmpty() {
     return this.first === this.last;
   }
+
+  /**
+   * @description 对队列进行扩容
+   * @author xieww
+   * @date 2020-10-13
+   * @param {*} length
+   * @memberof CircularQueue
+   */
   resize(length) {
     let q = new Array(length);
     for (let i = 0; i < length; i++) {
--- src/数据结构/队列/queue.js
+++ src/数据结构/队列/queue.js
@@ -8,18 +8,58 @@ class Queue {
   constructor() {
     this.queue = [];
   }
-  enQueue(item) {
-    this.queue.push(item);
+
+  /**
+   * @description 将一个元素放入队列的尾部
+   * @author xieww
+   * @date 2020-10-13
+   * @param {*} item
+   * @memberof Queue
+   */
+  push(value) {
+    this.queue.push(value);
   }
-  deQueue() {
+
+  /**
+   * @description 从队列首部移除元素
+   * @author xieww
+   * @date 2020-10-13
+   * @returns
+   * @memberof Queue
+   */
+  pop() {
     return this.queue.shift();
   }
-  getHeader() {
+
+  /**
+   * @description 返回队列首部的元素
+   * @author xieww
+   * @date 2020-10-13
+   * @returns
+   * @memberof Queue
+   */
+  peek() {
     return this.queue[0];
   }
+
+  /**
+   * @description 获取队列长度
+   * @author xieww
+   * @date 2020-10-13
+   * @returns
+   * @memberof Queue
+   */
   getLength() {
     return this.queue.length;
   }
+
+  /**
+   * @description 队列是否为空
+   * @author xieww
+   * @date 2020-10-13
+   * @returns {boolean}
+   * @memberof Queue
+   */
   isEmpty() {
     return this.getLength() === 0;
   }
