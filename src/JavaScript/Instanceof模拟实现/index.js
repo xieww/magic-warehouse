9d752f68447e8baa520b00af453e118f2226943a
Chasen.xie <chasen.xie@trustasia.com>
2020-10-19 17:28:01 +0800

    feat(JavaScript): JavaScript

--- src/JavaScript/Instanceof模拟实现/index.js
+++ src/JavaScript/Instanceof模拟实现/index.js
@@ -1,4 +1,4 @@
-function _Instanceof(left, right) {
+function _instanceof(left, right) {
   // 基本类型直接返回false
   if (typeof left !== "object" || !left) {
     return false;
@@ -23,9 +23,9 @@ class Student extends Person { }
 
 const student = new Student();
 
-console.log("should be true", _Instanceof(obj, Object));
-console.log("should be false", _Instanceof("123", String));
-console.log("should be true", _Instanceof(new String("111"), String));
-console.log("should be true", _Instanceof(student, Student));
-console.log("should be true", _Instanceof(student, Person));
-console.log("should be true", _Instanceof(new Number(123), Number));
+console.log("should be true", _instanceof(obj, Object));
+console.log("should be false", _instanceof("123", String));
+console.log("should be true", _instanceof(new String("111"), String));
+console.log("should be true", _instanceof(student, Student));
+console.log("should be true", _instanceof(student, Person));
+console.log("should be true", _instanceof(new Number(123), Number));
