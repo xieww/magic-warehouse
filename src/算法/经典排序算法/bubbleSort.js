2e519d7f66752d15969e176f6b25eaea658f07cc
Chasen.xie <chasen.xie@trustasia.com>
2020-11-12 16:10:46 +0800

    feat(sort): 快速排序

--- src/算法/经典排序算法/bubbleSort.js
+++ src/算法/经典排序算法/bubbleSort.js
@@ -20,13 +20,14 @@ function bubbleSort2(arr) {
   var i = arr.length - 1; //初始时,最后位置保持不变
   while (i > 0) {
     var pos = 0; //每趟开始时,无记录交换
-    for (var j = 0; j < i; j++)
+    for (var j = 0; j < i; j++) {
       if (arr[j] > arr[j + 1]) {
         pos = j; //记录交换的位置
         var tmp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = tmp;
       }
+    }
     i = pos; //为下一趟排序作准备
   }
   console.timeEnd("改进后冒泡排序耗时");
--- src/算法/经典排序算法/insertionSort.js
+++ src/算法/经典排序算法/insertionSort.js
@@ -1,4 +1,5 @@
 function insertionSort(arr) {
+  console.time("插入排序耗时：");
   var len = arr.length;
   var preIndex, current;
   for (var i = 1; i < len; i++) {
@@ -10,10 +11,37 @@ function insertionSort(arr) {
     }
     arr[preIndex + 1] = current;
   }
+  console.timeEnd("插入排序耗时：");
   return arr;
 }
 
+function binaryInsertionSort(array) {
+  console.time("优化后的二分插入排序耗时：");
+
+  for (var i = 1; i < array.length; i++) {
+    var key = array[i],
+      left = 0,
+      right = i - 1;
+    while (left <= right) {
+      var middle = parseInt((left + right) / 2);
+      if (key < array[middle]) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+    }
+    for (var j = i - 1; j >= left; j--) {
+      array[j + 1] = array[j];
+    }
+    array[left] = key;
+  }
+  console.timeEnd("优化后的二分插入排序耗时：");
+
+  return array;
+}
+var arr = [3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48];
 console.log(
   "should be [2, 3, 4, 5, 15, 19, 26, 27, 36, 38, 44, 46, 47, 48, 50]",
-  insertionSort([3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48])
+  insertionSort(arr)
 );
+console.log(binaryInsertionSort(arr));
