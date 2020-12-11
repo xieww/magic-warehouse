bfd890bd3b6dd2fdd3f8ecb448a6d831d6d1e577
Chasen.xie <chasen.xie@trustasia.com>
2020-12-11 11:23:25 +0800

    feat(JavaScript): 模板引擎

---
+++ src/JavaScript/模板引擎/render.js
@@ -0,0 +1,28 @@
+function render(template, data) {
+  let str = template;
+  for (const key in data) {
+    const regex = new RegExp("{{" + key + "}}", "g");
+    str = str.replace(regex, data[key]);
+  }
+  return str;
+}
+
+function renders(template, data) {
+  Object.keys(data).forEach((key) => {
+    template = template.replace(new RegExp("{{" + key + "}}", "g"), data[key]);
+  });
+  return template;
+}
+
+function renderTemplate(template, data) {
+  const regex = /\{\{(.*?)\}\}/g; // '/\{\{(\w+)\}\}/g'
+  return template.replace(regex, (match, key) => data[key.trim()]);
+}
+
+const template = "我的名字是{{name }},今年{{ age }}。";
+const obj = {
+  name: "wu",
+  age: "18",
+};
+
+console.log(renderTemplate(template, obj));
