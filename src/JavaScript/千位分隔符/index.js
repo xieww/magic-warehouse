080bd5edb43f8347539533dfec91fbb2433679e5
Chasen.xie <chasen.xie@trustasia.com>
2021-03-22 14:38:25 +0800

    feat(JavaScript): 千位分隔符

--- src/JavaScript/千位分隔符/index.js
+++ src/JavaScript/千位分隔符/index.js
@@ -47,11 +47,20 @@
 //   });
 // }
 
+// function numberFormat(num) {
+//   return (
+//     num &&
+//     num.toString().replace(/\d+/, function (s) {
+//       return s.replace(/(\d)(?=(\d{3})+$)/g, "$1,");
+//     })
+//   );
+// }
+
 function numberFormat(num) {
   return (
     num &&
     num.toString().replace(/\d+/, function (s) {
-      return s.replace(/(\d)(?=(\d{3})+$)/g, "$1,");
+      return s.replace(/(?=(\B\d{3})+$)/g, ",");
     })
   );
 }
