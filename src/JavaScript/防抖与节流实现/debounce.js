482261eac0720312a8f8d72958b808e4996fdaca
Chasen.xie <chasen.xie@trustasia.com>
2021-03-22 09:46:04 +0800

    feat(JavaScript): 防抖函数实现

--- src/JavaScript/防抖与节流实现/debounce.js
+++ src/JavaScript/防抖与节流实现/debounce.js
@@ -17,3 +17,33 @@ function debounce(fn, delay = 500) {
     }, delay);
   };
 }
+
+function debounce(fn, delay = 500, immediate) {
+  let timer = null;
+  let result = "";
+  var debounced = function (...args) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    if (immediate) {
+      // 若已经执行过，不再执行
+      const callNow = !timer;
+      timer = setTimeout(() => {
+        timer = null;
+      }, delay);
+      if (callNow) {
+        result = fn.apply(this, args);
+      }
+    } else {
+      timer = setTimeout(() => {
+        result = fn.apply(this.args);
+      }, delay);
+    }
+    return result;
+  };
+  debounced.cancel = function () {
+    clearTimeout(timer);
+    timer = null;
+  };
+  return debounced;
+}
