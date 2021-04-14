a5a95ccd828149d8d86471a4037bdf78af47bd22
Chasen.xie <chasen.xie@trustasia.com>
2021-04-14 14:11:16 +0800

    feat(JavaScript): 36进制加法

--- src/JavaScript/36进制加法/index.js
+++ src/JavaScript/36进制加法/index.js
@@ -37,6 +37,9 @@ const alphabet = [
   "Z",
 ];
 function addFor36(str1, str2) {
+  if (!str1 || !str2) {
+    return str1 || str2;
+  }
   const len1 = str1.length;
   const len2 = str2.length;
   let i = len1 - 1;
@@ -47,10 +50,10 @@ function addFor36(str1, str2) {
     let s = alphabet.indexOf(str1[i]) + alphabet.indexOf(str2[j]) + carry;
     if (s >= 36) {
       carry = 1;
-      result = alphabet[s % 36] + result;//有进位将余数和上一次结果相加
+      result = alphabet[s % 36] + result; //有进位将余数和上一次结果相加
     } else {
       carry = 0;
-      result = alphabet[s] + result;//无进位直接和上一次结果相加
+      result = alphabet[s] + result; //无进位直接和上一次结果相加
     }
     i--;
     j--;
@@ -84,4 +87,4 @@ function addFor36(str1, str2) {
 }
 
 console.log(addFor36("AB", "Z"));
-console.log(addFor36("H", "WG"));
+console.log(addFor36("ZH", "WG"));
