420dfc25fb0ced740c75c95d5a4598e4266a5bad
Chasen.xie <chasen.xie@trustasia.com>
2021-04-20 12:56:36 +0800

    feat(JavaScript): promisify

--- src/JavaScript/promisify实现/index.js
+++ src/JavaScript/promisify实现/index.js
@@ -1,6 +1,6 @@
 /**
  * promisify：就是“promise 化”，将一个不是promise的方法变成 promise
- * nodeCallback: 
+ * nodeCallback:
  *    1. 回调函数在主函数中的参数位置必须是最后一个;
  *    2. 回调函数参数中的第一个参数必须是 error
  * @param {*} func
@@ -30,6 +30,34 @@ var promisify = (func, ctx) => {
     });
   };
 };
+
+{
+  var promisify = function (method, ctx) {
+    return function () {
+      //获取method调用的需要参数
+      var args = Array.prototype.slice.call(arguments, 0);
+
+      // use runtime this if ctx not provided
+      ctx = ctx || this;
+
+      //返回一个新的Promise对象
+      return new Promise(function (resolve, reject) {
+        //除了函数传入的参数以外还需要一个callback函数来供异步方法调用
+        var callback = function () {
+          return function (err, result) {
+            if (err) {
+              return reject(err);
+            }
+            return resolve(result);
+          };
+        };
+        args.push(callback());
+        //调用method
+        method.apply(ctx, args);
+      });
+    };
+  };
+}
 
 // nodeCallback方法func1
 var func1 = function (a, b, c, callback) {
