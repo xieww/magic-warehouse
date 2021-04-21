798c9f843c9a96fcd20b26effc193d952177d012
Chasen.xie <chasen.xie@trustasia.com>
2021-04-21 10:55:24 +0800

    feat(ajax): 封装ajax请求

--- src/JavaScript/异步请求/ajax.js
+++ src/JavaScript/异步请求/ajax.js
@@ -1,16 +1,25 @@
-const getJSON = function(url) {
+const DEFAULT_CONFIG = { method: "GET", async: true };
+const request = function (url, config) {
+  const { method, async, headers, data } = config || DEFAULT_CONFIG;
   return new Promise((resolve, reject) => {
-    const xhr = XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHttp');
-    xhr.open('GET', url, false);
-    xhr.setRequestHeader('Accept', 'application/json');
-    xhr.onreadystatechange = function() {
+    const xhr = XMLHttpRequest
+      ? new XMLHttpRequest()
+      : new ActiveXObject("Microsoft.XMLHttp");
+    xhr.open(method, url, async);
+    xhr.setRequestHeader("Accept", "application/json");
+    if (headers) {
+      Object.keys(headers).forEach((key) => {
+        xhr.setRequestHeader(key, headers[key]);
+      });
+    }
+    xhr.onreadystatechange = function () {
       if (xhr.readyState !== 4) return;
       if (xhr.status === 200 || xhr.status === 304) {
         resolve(xhr.responseText);
       } else {
         reject(new Error(xhr.responseText));
       }
-    }
-    xhr.send();
-  })
-}
+    };
+    xhr.send(data);
+  });
+};
