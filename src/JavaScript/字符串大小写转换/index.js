ce6ce79a708be8e9a859a6e4ce16e514aca11601
Chasen.xie <chasen.xie@trustasia.com>
2021-05-28 17:13:31 +0800

    feat(JavaScript): 字符串大小写转换

---
+++ src/JavaScript/字符串大小写转换/index.js
@@ -0,0 +1,20 @@
+var str = "AbC";
+function transfer(str) {
+  // let result = "";
+  // for (const item of str) {
+  //   const lower = item.toLocaleLowerCase();
+  //   const upper = item.toLocaleUpperCase();
+  //   if (upper === item) {
+  //     result += lower;
+  //   } else {
+  //     result += upper;
+  //   }
+  // }
+  // return result;
+
+  return str.replace(/[a-zA-Z]/g, (match) => {
+    return /[a-z]/.test(match) ? match.toUpperCase() : match.toLowerCase();
+  });
+}
+
+console.log(transfer(str));
