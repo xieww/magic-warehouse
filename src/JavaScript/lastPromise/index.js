12a1802e51e1aa231e0f3395b067873a9bdb1442
xieww <2801416900@qq.com>
2021-11-02 23:35:22 +0800

    feat(promise): lastPromise

---
+++ src/JavaScript/lastPromise/index.js
@@ -0,0 +1,47 @@
+function lastPromise(promiseFunction) {
+  lastPromise.target1 = 0;
+  lastPromise.target2 = 0;
+
+  return function () {
+    lastPromise.target1++;
+    return new Promise((resolve, reject) => {
+      promiseFunction().then((v) => {
+        lastPromise.target2++;
+        if (lastPromise.target1 === lastPromise.target2) {
+          resolve(v);
+        }
+      });
+    });
+  };
+}
+
+// function lastPromise(promiseFunction) {
+//   const cbs = [];
+//   const promiseMap = {};
+//   return () => {
+//     return new Promise((resolve, reject) => {
+//       cbs.push(resolve);
+//       const p = promiseFunction();
+//       promiseMap[p] = resolve;
+//       p.then((r) => {
+//         if (cbs.indexOf(resolve) !== cbs.length - 1) {
+//           return;
+//         }
+//         resolve(r);
+//       });
+//     });
+//   }
+// }
+
+// 示例
+let count = 1;
+let promiseFunction = () =>
+  new Promise((rs) =>
+    setTimeout(() => {
+      rs(count++);
+    })
+  );
+let lastFn = lastPromise(promiseFunction);
+lastFn().then(console.log); // 无输出
+lastFn().then(console.log); // 无输出
+lastFn().then(console.log); // 3
