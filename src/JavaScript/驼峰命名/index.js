02dc3ea0254eb9e2401d06996b043f80ff7a629a
Chasen.Xie <chasen.xie@trustasia.com>
2021-11-11 08:28:05 +0800

    feat(JavaScript): 驼峰命名

--- src/JavaScript/驼峰命名/index.js
+++ src/JavaScript/驼峰命名/index.js
@@ -4,5 +4,15 @@ function transfer(str) {
   });
 }
 
+function transfer2(str) {
+  const arr = str.split("-");
+  return arr
+    .map((item, index) =>
+      index === 0 ? item : item[0].toUpperCase() + item.slice(1)
+    )
+    .join("");
+}
+
 var s1 = "get-element-by-id";
 console.log(transfer(s1));
+console.log(transfer2(s1));
